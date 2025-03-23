ea299498b27277d2a14a74c84f6dd6310120dbe2
Ninon <ninon.jordao@outlook.fr>
2025-03-23 13:14:01 +0100

    modification / suppression de badge

--- src/controllers/badgeController.js
+++ src/controllers/badgeController.js
@@ -1,5 +1,6 @@
 const Badge = require("../models/Badge");
 const Player = require("../models/Player");
+const mongoose = require("mongoose");
 
 exports.createBadge = async (req, res) => {
   try {
@@ -85,6 +86,61 @@ exports.removeBadgeFromPlayer = async (req, res) => {
     player.badges.pull(badgeId);
     await player.save();
     res.status(200).json({ message: "Badge supprimé du joueur avec succès" });
+  } catch (error) {
+    console.error("Erreur lors de la suppression du badge:", error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la suppression du badge", error });
+  }
+};
+
+exports.updateBadge = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, imageUrl, description } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID de badge invalide" });
+    }
+
+    const badge = await Badge.findById(id);
+    if (!badge) {
+      return res.status(404).json({ message: "Badge non trouvé" });
+    }
+
+    badge.title = title;
+    badge.imageUrl = imageUrl;
+    badge.description = description;
+
+    await badge.save();
+
+    res.status(200).json(badge);
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour du badge:", error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la mise à jour du badge", error });
+  }
+};
+
+exports.deleteBadge = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID de badge invalide" });
+    }
+
+    // Supprimer le badge
+    const deletedBadge = await Badge.findByIdAndDelete(id);
+    if (!deletedBadge) {
+      return res.status(404).json({ message: "Badge non trouvé" });
+    }
+
+    // Supprimer les références à ce badge dans tous les joueurs qui le possèdent
+    await Player.updateMany({ badges: id }, { $pull: { badges: id } });
+
+    res.status(200).json({ message: "Badge supprimé avec succès" });
   } catch (error) {
     console.error("Erreur lors de la suppression du badge:", error);
     res
--- src/routes/badgeRoutes.js
+++ src/routes/badgeRoutes.js
@@ -5,12 +5,16 @@ const {
   getBadgeById,
   assignBadgeToPlayer,
   removeBadgeFromPlayer,
+  updateBadge,
+  deleteBadge,
 } = require("../controllers/badgeController");
 const router = express.Router();
 
 router.post("/", createBadge);
 router.get("/", getBadges);
 router.get("/:id", getBadgeById);
+router.put("/:id", updateBadge); // Ajouter cette route
+router.delete("/:id", deleteBadge); // Ajouter cette route
 router.post("/assign", assignBadgeToPlayer);
 router.post("/remove", removeBadgeFromPlayer);
 
